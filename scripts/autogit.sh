6905726502b78a6883f7f1f9146b41ddbb254c27
shoaibb <shoaibb@users.noreply.github.com>
2023-05-30 12:55:18 +0000

    deploy: shoaibb/src_notes@3347922ffc579c901de1065770901ef97c19e90a

--- scripts/autogit.sh
+++ scripts/autogit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "Commit message: " desc
-source='/Users/SBAKHTYA/iMind/RandomBits'
+source='/Users/SBAKHTYA/iMind/RandomBits/'
 dest='/Users/SBAKHTYA/projects/gitp/src_notes/content/posts'
-cp $source/*.md $dest/
+rsync -avu $source $dest
 cd '/Users/SBAKHTYA/projects/gitp/src_notes'
 git add .
 git add -u
