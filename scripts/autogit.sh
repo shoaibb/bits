e2edafd0a0a2b492308bfb51bf248d9a6429218f
shoaibb <shoaibb@users.noreply.github.com>
2023-05-30 07:55:09 +0000

    deploy: shoaibb/src_notes@c614a7f367af7b96ed1f095920247373cd9b73c2

--- scripts/autogit.sh
+++ scripts/autogit.sh
@@ -5,5 +5,6 @@ dest='/Users/SBAKHTYA/projects/gitp/src_notes/content/posts'
 cp $source/*.md $dest/
 cd '/Users/SBAKHTYA/projects/gitp/src_notes'
 git add .
+git add -u
 git commit -m "$desc"
 git push
