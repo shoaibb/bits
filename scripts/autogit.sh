ea855e98fcc3fa9611f8d151b5de9501e667638e
shoaibb <shoaibb@users.noreply.github.com>
2023-05-30 07:44:38 +0000

    deploy: shoaibb/src_notes@ccc90c655d99249df9ff6e1371d78e00e048fa18

--- scripts/autogit.sh
+++ scripts/autogit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+read -p "Commit message: " desc
 source='/Users/SBAKHTYA/iMind/RandomBits'
 dest='/Users/SBAKHTYA/projects/gitp/src_notes/content/posts'
 cp $source/*.md $dest/
 cd '/Users/SBAKHTYA/projects/gitp/src_notes'
 git add .
-git commit -a -m "$1"
+git commit -m "$desc"
 git push
